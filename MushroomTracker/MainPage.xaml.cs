0ec3c8d34c228665a99bd6f119842d1d539e268c
PKpja <s7458@pjwstk.edu.pl>
2015-06-15 11:59:30 -0700

    navigate to current position on a map

--- MushroomTracker/MainPage.xaml.cs
+++ MushroomTracker/MainPage.xaml.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Geolocation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,37 +15,60 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
-
+using Windows.UI.Xaml.Shapes;
+ 
 namespace MushroomTracker
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
+ 
     public sealed partial class MainPage : Page
     {
+        private CoreDispatcher dispatcher;
+        private Geolocator geolocator;
+        private Geoposition geoposition;
+
         public MainPage()
         {
             this.InitializeComponent();
 
             this.NavigationCacheMode = NavigationCacheMode.Required;
-        }
 
-        /// <summary>
-        /// Invoked when this page is about to be displayed in a Frame.
-        /// </summary>
-        /// <param name="e">Event data that describes how this page was reached.
-        /// This parameter is typically used to configure the page.</param>
+            geolocator = new Geolocator();
+            dispatcher = Window.Current.CoreWindow.Dispatcher;  
+        }
+         
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            // TODO: Prepare page for display here.
+            
+            refreshLocation();
 
-            // TODO: If your application contains multiple pages, ensure that you are
-            // handling the hardware Back button by registering for the
-            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
-            // If you are using the NavigationHelper provided by some templates,
-            // this event is handled for you.
+            geolocator.MovementThreshold = 3;  
+            geolocator.PositionChanged +=
+            new TypedEventHandler<Geolocator,
+                PositionChangedEventArgs>(geolocator_PositionChanged);  
+
         }
+
+        async private void geolocator_PositionChanged(Geolocator sender, PositionChangedEventArgs e)
+        {
+           
+           await dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+           {
+               refreshLocation();
+           });    
+
+        } 
+
+        private async void refreshLocation() {
+            try
+            {
+                geoposition = await geolocator.GetGeopositionAsync();
+                mapControl.Center = geoposition.Coordinate.Point;
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        
     }
 }
